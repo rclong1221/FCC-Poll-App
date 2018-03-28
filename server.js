fa436bc78814a0a0c671107b0c32e3544d1c80d0
R.C. Long <deac.long@wsu.edu>
2018-03-28 03:09:54 -0700

    Add view engine and dependencies

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var path = require('path')
 var express = require('express');
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
+var handlebars = require('handlebars')
 
 var app = express();
 require('dotenv').load();
@@ -12,6 +14,16 @@ require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
+
+// View engine
+app.engine('handlebars', handlebars({
+	extname: 'handlebars',
+	defaultLayout: 'layout',
+	layoutsDir: __dirname + '/app/views/layouts'
+}))
+app.set('views', path.join(__dirname, 'app/views'))
+app.set('view engine', 'handlebars')
+
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
