0b71c4a0cf5289f29ce1d948d559a97da993f967
R.C. Long <deac.long@wsu.edu>
2018-03-29 04:26:28 -0700

    Remove semi-colons

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
-'use strict';
+'use strict'
 
 const path = require('path')
-const express = require('express');
-const routes = require('./app/routes/index.js');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const session = require('express-session');
+const express = require('express')
+const routes = require('./src/routes/index.js')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
 const handlebars = require('express-handlebars')
 
-const app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
+const app = express()
+require('dotenv').load()
+require('./src/lib/passport')(passport)
 
-mongoose.connect(process.env.MONGO_URI);
-mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI)
+mongoose.Promise = global.Promise
 
 // View engine
 app.engine('handlebars', handlebars({
 	extname: 'handlebars',
-	defaultLayout: 'layout',
-	layoutsDir: __dirname + '/app/views/layouts'
+	// defaultLayout: 'layout',
+	// layoutsDir: __dirname + '/src/views/layouts'
 }))
-app.set('views', path.join(__dirname, 'app/views'))
+app.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'handlebars')
 
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+app.use('/controllers', express.static(process.cwd() + '/src/controllers'))
+app.use('/public', express.static(process.cwd() + '/public'))
+app.use('/common', express.static(process.cwd() + '/src/common'))
 
 app.use(session({
 	secret: 'secretClementine',
 	resave: false,
 	saveUninitialized: true
-}));
+}))
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-routes(app, passport);
+routes(app, passport)
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080
 app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
-});
+	console.log('Node.js listening on port ' + port + '...')
+})
