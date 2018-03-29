32da391f51899a4fb7553b48e1d4f467b4c5fde6
R.C. Long <deac.long@wsu.edu>
2018-03-29 09:21:01 -0700

    Remove console.logs

--- src/controllers/pollsController.client.js
+++ src/controllers/pollsController.client.js
@@ -12,11 +12,9 @@ $(document).ready(getData());
 
 function getData() {
   $.get( "/api/polls", function( data ) {
-    console.log(data);
     var h = "";
     for (var i = 0; i < data.length; i++) {
       var entry = p1 + p2 + p3 + data[i]._id + p4 + data[i].question + p5;
-      console.log(entry);
       $("#p-c").append(entry);
     }
   });
