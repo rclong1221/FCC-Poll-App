a03ca979126ab586a2e798cdaadf7fe86829d3b5
R.C. Long <deac.long@wsu.edu>
2018-04-06 07:37:35 -0700

    Remove stock logo

--- src/controllers/userController.client.js
+++ src/controllers/userController.client.js
@@ -1,12 +1,8 @@
 'use strict';
 
 $(document).ready(function () {
-  getUser();
-});
-
-function getUser() {
   $.get("/api/user/:id", function (d, s) {
     if (d.twitter || d.github) $("#lio").attr("href", "/logout").html("Logout");
     else $("#lio").attr("href", "/login").html("Login");
   })
-}
+});
